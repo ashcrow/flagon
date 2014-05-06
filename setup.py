4585587d44761dc5826a92f4a4c7d52471a61180
Steve Milner <stevem@gnulinux.net>
2014-05-06 19:34:27 -0400

    Added build items.

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# Copyright (C) 2014 Steve Milner
+"""
+Build script.
+"""
+
+import os.path
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='flagon',
+    version='0.0.1',
+    author='Steve Milner',
+    url='https://github.com/ashcrow/flagon',
+    license='MIT',
+    zip_safe=False,
+    package_dir={
+        'flagon': os.path.join('src', 'flagon')
+    },
+    packages=find_packages('src'),
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python',
+    ],
+
+)
