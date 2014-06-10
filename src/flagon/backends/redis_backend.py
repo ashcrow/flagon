8a60d862131b525821db1eff2ed9c9d3a09a0712
Steve Milner <stevem@gnulinux.net>
2014-06-10 13:49:22 -0400

    Minor updates to RedisBackend.

--- src/flagon/backends/redis_backend.py
+++ src/flagon/backends/redis_backend.py
@@ -1,26 +1,24 @@
-import warnings
 import redis
-'''
-warnings.warn('RedisBackend is not safe for '
-              'multi-thread/process environments')
-'''
+
 from flagon import errors
 from flagon.backends import Backend
 
+
 class RedisBackend(Backend):
 
-    def __init__(self, connection_str):
+    def __init__(self, host, port, db):
         """
         Creates an instance of the RedisBackend.
 
-        :param connection_str: information can be found at
-            https://pypi.python.org/pypi/redis/2.10.1
-        :type connection_str: str
         :rtype: RedisBackend
         """
         # https://pypi.python.org/pypi/redis/2.10.1
-        pool = redis.ConnectionPool(host='127.0.0.1', port=6379, db=0)
-        self._server = redis.Redis(connection_pool=pool, charset='utf-8', errors='strict', decode_responses=False)
+        pool = redis.ConnectionPool(host=host, port=port, db=db)
+        self._server = redis.Redis(
+            connection_pool=pool,
+            charset='utf-8',
+            errors='strict',
+            decode_responses=False)
 
     def set(self, name, key, value):
         """
@@ -64,7 +62,8 @@ class RedisBackend(Backend):
         """
         # TODO: Copy paste --- :-(
         if not self._server.hexists(name, key):
-            raise errors.UnknownFeatureError('Unknown feature: %s %s' %(name,key))
+            raise errors.UnknownFeatureError('Unknown feature: %s %s' % (
+                name, key))
         self._server.hset(name, key, value)
 
     turn_on = lambda s, name: s._turn(name, 'active', True)
